a67555c33a7335e8110e5dab50cc5968e0e9367e
ian-dcg <ianndcg@gmail.com>
2022-12-23 17:42:58 -0300

    Adicionando responsividade e finalizando navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,16 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import {
+  MobileIcon,
+  Nav,
+  NavbarContainer,
+  NavItem,
+  NavLinks,
+  NavLogo,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
 const Navbar = () => {
   return (
@@ -7,6 +18,26 @@ const Navbar = () => {
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">dolla</NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to="about">About</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="discover">Discover</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="services">Services</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to="signup">Sign Up</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to="/signin">Sign In</NavBtnLink>
+          </NavBtn>
         </NavbarContainer>
       </Nav>
     </>
